0051e6fc4b97dd63dce3e7ba151f5349ce60fdbb
Dan Crowe <danielcrowe@halesowen.ac.uk>
2022-11-08 10:56:40 +0000

    Updated button to respect brand

--- resources/js/components/button.jsx
+++ resources/js/components/button.jsx
@@ -6,11 +6,11 @@ const Button = ({ as: Tag = Link, color = "default", children, className, ...res
     <Tag
         {...rest}
         className={classNames(
-            "inline-flex items-center rounded-md border px-5 py-2.5 text-sm font-medium shadow-sm focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2",
+            "inline-flex items-center rounded-md border px-5 py-2.5 text-sm font-medium shadow-sm focus:outline-none focus:ring-2 focus:ring-brand-500 focus:ring-offset-2",
             {
                 // Colors
                 "border-gray-300 bg-white text-gray-700 hover:bg-gray-50": color === "default",
-                "border-transparent bg-indigo-600 text-white hover:bg-indigo-700": color === "brand",
+                "border-transparent bg-brand-600 text-white hover:bg-brand-700": color === "brand",
             },
             className
         )}
