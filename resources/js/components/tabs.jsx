78681a74d488c1f936422bcd8a6db819fab26759
Dan Crowe <danielcrowe@halesowen.ac.uk>
2022-11-15 15:30:48 +0000

    Updated active tab styles

--- resources/js/components/tabs.jsx
+++ resources/js/components/tabs.jsx
@@ -10,9 +10,11 @@ const Tabs = ({ tabs, activeTab, onTabChange, className, ...rest }) => (
                     type="button"
                     className={classNames(
                         "rounded-md px-3 py-1.5 text-sm font-medium outline-none focus:outline-brand-500",
-                        activeTab && activeTab === tab.value
-                            ? "bg-brand-100 text-brand-700"
-                            : "text-gray-500 hover:text-gray-700"
+                        {
+                            // Active
+                            "bg-brand-100 text-brand-700": activeTab && activeTab === tab.value,
+                            "text-gray-500 hover:text-gray-700": !activeTab || activeTab !== tab.value,
+                        }
                     )}
                     onClick={() => onTabChange(tab)}
                 >
