45ac975a6e700ad66f04e234ff8dd412b466beee
Dan Crowe <danielcrowe@halesowen.ac.uk>
2022-11-15 15:10:41 +0000

    Added tabs focus state

--- resources/js/components/tabs.jsx
+++ resources/js/components/tabs.jsx
@@ -9,7 +9,7 @@ const Tabs = ({ tabs, activeTab, onTabChange, className, ...rest }) => (
                     key={tab.value}
                     type="button"
                     className={classNames(
-                        "rounded-md px-3 py-1.5 text-sm font-medium",
+                        "rounded-md px-3 py-1.5 text-sm font-medium outline-none focus:outline-brand-500",
                         activeTab && activeTab === tab.value
                             ? "bg-brand-100 text-brand-700"
                             : "text-gray-500 hover:text-gray-700"
