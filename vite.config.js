7173d3751b4b1b1982fd9edf9d05c61314e13a1e
Dan Crowe <danielcrowe@halesowen.ac.uk>
2022-11-21 12:09:07 +0000

    Added favicon

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,7 @@ import react from "@vitejs/plugin-react";
 import fs from "fs";
 import laravel from "laravel-vite-plugin";
 import { defineConfig } from "vite";
+import { viteStaticCopy } from "vite-plugin-static-copy";
 
 const host = "rankup.local";
 const certPath = "./certificates/development.pfx";
@@ -14,6 +15,14 @@ export default defineConfig({
             refresh: true,
         }),
         react(),
+        viteStaticCopy({
+            targets: [
+                {
+                    src: "./resources/static/**/*",
+                    dest: "../",
+                },
+            ],
+        }),
     ],
     server: {
         host,
